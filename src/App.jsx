1bdc7d549a158bfedb66629e51c8d0d1f7c47ed4
Yuriy Nabasov <nabasoff@gmail.com>
2025-07-28 19:33:57 +0300

    add-lazy-pages-import

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 // src/App.jsx
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { loadFavorites } from "./redux/favoritesSlice";
+import { lazy } from "react";
 
 import Header from "./components/Header/Header.jsx";
+import Loader from "./components/Loader/Loader.jsx";
 
 import HomePage from "./pages/HomePage/HomePage.jsx";
-import CatalogPage from "./pages/CatalogPage/CatalogPage.jsx";
-import CarDetailsPage from "./pages/CarDetailsPage/CarDetailsPage.jsx";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
+
+const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage.jsx"));
+const CarDetailsPage = lazy(() =>
+  import("./pages/CarDetailsPage/CarDetailsPage.jsx")
+);
+const NotFoundPage = lazy(() =>
+  import("./pages/NotFoundPage/NotFoundPage.jsx")
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -22,12 +29,14 @@ function App() {
     <>
       <Header />
       <div className="content-wrapper">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<CarDetailsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/catalog/:id" element={<CarDetailsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </div>
     </>
   );
